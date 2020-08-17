eafdbccc6df87618b4194291f7769a57ed54801e
Ivor Padilla <ivorjpc@gmail.com>
2020-08-17 12:50:15 +0200

    added deploy url webhook to env variables

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,15 @@
 # set environment variable in vercel
 # trigger deploy hook in vercel
 
-echo "Fetching access token"
-# token=$(curl -k -H "Content-Type:application/json" -X POST -d  '{ "user":"admin@dotcms.com", "password":"admin", "expirationDays": 10 }' https://demo.dotcms.com/api/v1/authentication/api-token | ./jq -r '.entity.token')
- 
-echo "Setting Vercel environment variable"
+read_var() {
+    VAR=$(grep $1 $2 | xargs)
+    IFS="=" read -ra VAR <<< "$VAR"
+    echo ${VAR[1]}
+}
 
+DEPLOY_ENDPOINT=$(read_var DEPLOY_ENDPOINT .env)
+
+echo "Removing existing Vercel environment variable"
 expect <<- DONE
   set timeout -1
 
@@ -24,14 +28,20 @@ expect <<- DONE
   expect eof
 DONE
 
+echo "Fetching new access token"
+token=$(curl -k -H "Content-Type:application/json" -X POST -d  '{ "user":"admin@dotcms.com", "password":"admin", "expirationDays": 10 }' https://demo.dotcms.com/api/v1/authentication/api-token | ./jq -r '.entity.token')
+ 
+echo "Setting Vercel environment variable"
 expect <<- DONE
  set timeout -1
 
  spawn vercel env add BEARER_TOKEN 
  expect "What’s the value of BEARER_TOKEN?"
- send -- "123\r"
+ send -- "$token\r"
  sleep 1
  send "a"
  send -- "\n"
+ sleep 1
+ spawn curl -X POST $DEPLOY_ENDPOINT
  expect eof
 DONE
